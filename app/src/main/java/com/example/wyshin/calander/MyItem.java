688dfd27ff589f99e9432627d6f93dbd78daf409
jieunKimm <qorentks7777@gmail.com>
2017-06-20 01:16:41 +0900

    calander

---
+++ app/src/main/java/com/example/wyshin/calander/MyItem.java
@@ -0,0 +1,43 @@
+package com.example.wyshin.calander;
+
+import android.graphics.drawable.Drawable;
+
+public class MyItem {
+
+    private Drawable icon;
+    private String name;
+    private String contents;
+    private String time;
+
+    public Drawable getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Drawable icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/com/example/wyshin/calander/ShowScheduleDetail.java
@@ -0,0 +1,63 @@
+package com.example.wyshin.calander;
+
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ShowScheduleDetail extends AppCompatActivity {
+    private String[] schedule_array = new String[32];
+    private String[][] time = new String[32][2];
+    private ListView mListView;
+    private int day;
+    private String temp;
+    private int get_day;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_schedule_detail);
+        //intent함수를 이용하여 key값을 이용해 원하는 값을 받는다.
+        Intent intent = getIntent();
+        get_day = intent.getExtras().getInt("day");
+
+        mListView = (ListView)findViewById(R.id.listView);
+
+        /* 아이템 추가 및 어댑터 등록 */
+        dataSetting();
+    }
+    private void dataSetting() {
+        MyAdapter mMyAdapter = new MyAdapter();
+
+        try {
+            FileInputStream fis = openFileInput("data9.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String str;
+            while (true) {
+                temp = br.readLine();
+                if (temp != null) {
+                    StringTokenizer st = new StringTokenizer(temp);
+                    day = Integer.parseInt(st.nextToken("/"));
+                    String schedule = st.nextToken("/");
+                    schedule_array[day] = schedule;
+                    time[day][0] = st.nextToken("/");
+                    time[day][1] = st.nextToken("/");
+                } else
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMyAdapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.clock), get_day, schedule_array[get_day],time[day][0],time[day][1]);
+
+
+        /* 리스트뷰에 어댑터 등록 */
+        mListView.setAdapter(mMyAdapter);
+    }
+}
+
